1c6594f58cbfb34688f078cc2ade34dd077df7e8
Ricardo Franco <ricardofranco14@gmail.com>
2021-12-13 12:02:30 +0000

    Setting up the back-end part

--- routes/upload.js
+++ routes/upload.js
@@ -12,7 +12,7 @@ cloudinary.config({
 });
 
 // upload image only admin can use
-router.post("/upload", (req, res) => {
+router.post("/upload", auth, authAdmin, (req, res) => {
   try {
     console.log(req.files);
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -45,7 +45,7 @@ router.post("/upload", (req, res) => {
 })
 
 // delete image only admin can use
-router.post("/destroy", (req, res) => {
+router.post("/destroy", auth, authAdmin, (req, res) => {
   try {
     const { public_id } = req.body;
     if (!public_id) {
