1a412bb42fa474a54c6d8c05fc563161264eb7e5
Ricardo Franco <ricardofranco14@gmail.com>
2021-12-22 23:51:28 +0000

    Final Deploy

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-// const app = require("./app");
-
-// // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-// const PORT = process.env.PORT || 5005;
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port n`);
-// });
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express()
 app.use(express.json());
@@ -40,6 +32,13 @@ mongoose.connect(URI, {
   if (err) throw err;
   console.log('Connected to MongoDB')
 })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
