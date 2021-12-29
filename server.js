4b3cde76374f3096c833745c2bb65cd0990b61e5
Ricardo Franco <ricardofranco14@gmail.com>
2021-12-29 19:15:56 +0000

    Final deploy

--- server.js
+++ server.js
@@ -33,13 +33,14 @@ mongoose.connect(URI, {
   console.log('Connected to MongoDB')
 })
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   })
+// }
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+})
