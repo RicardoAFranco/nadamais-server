488d3ce084ab86e762564ffa9f30c42c44c76ff4
Ricardo Franco <ricardofranco14@gmail.com>
2021-12-29 16:00:20 +0000

    Final deploy

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-// const app = require("./app");
-
-// // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-// const PORT = process.env.PORT || 5005;
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port n`);
-// });
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
